2d8b4320af8345adfaa605456539715d84d6aead
Студент группы IS <is@sielom.local>
2025-11-14 09:49:38 +0500

    add html

--- admin/admin/urls.py
+++ admin/admin/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('web/', TemplateView.as_view(template_name='web.html'), name='web'),
+    path('', TemplateView.as_view(template_name='web.html'), name='home'),
 ]
